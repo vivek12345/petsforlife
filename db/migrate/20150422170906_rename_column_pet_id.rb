6b6bc92ada495d07706742e2fb9e0b1a18425cca
Vivek Nayyar <viveknayyar@Viveks-MacBook-Pro-3.local>
2015-07-22 06:09:28 +0530

    removed the edit pet_id migration and removed rop table of breed migration

--- db/migrate/20150422170906_rename_column_pet_id.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnPetId < ActiveRecord::Migration
-  def change
-  	rename_column :breeds,:pet_id,:pet_name
-  	change_column :breeds,:pet_name,:string
-  end
-end
--- db/migrate/20150714230123_create_breeds.rb
+++ db/migrate/20150714230123_create_breeds.rb
@@ -1,6 +1,5 @@
 class CreateBreeds < ActiveRecord::Migration
   def change
-    drop_table :breeds
     create_table :breeds do |t|
       t.string :name
       t.string :pet_name
