139addafe3fb3d353f31d91bfce557ef68564da9
Vivek Nayyar <viveknayyar@Viveks-MacBook-Pro-3.local>
2015-07-30 16:12:47 +0530

    search title corrected and now if user accnt deleted ,all his listings will too

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ActiveRecord::Base
 	#has_attachments :photos, maximum: 10
     include UuidHelper
 	has_many :photos
-    belongs_to :user
+    belongs_to :user,:dependent => :destroy
     belongs_to :favourite,:dependent => :destroy
     validates_presence_of :title,:pet_type,:love_for_pets
     validates_numericality_of :price
