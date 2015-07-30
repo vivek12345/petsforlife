31fb5ab539ee9c10ecfacb2e1dd85ec0ed4ed9ee
Vivek Nayyar <viveknayyar@Viveks-MacBook-Pro-3.local>
2015-07-31 00:17:49 +0530

    depended destroy condition changed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,8 @@ class Listing < ActiveRecord::Base
 	#has_attachments :photos, maximum: 10
     include UuidHelper
 	has_many :photos
-    belongs_to :user,:dependent => :destroy
-    belongs_to :favourite,:dependent => :destroy
+    belongs_to :user
+    # belongs_to :favourite,:dependent => :destroy
     validates_presence_of :title,:pet_type,:love_for_pets
     validates_numericality_of :price
     validates_length_of :phone_no, :maximum => 11
